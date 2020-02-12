55e6098876abb0cb35a07d614107e7ce767aca70
redsudo <34559012+redsudo@users.noreply.github.com>
2020-02-11 23:17:34 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='spoofbuz',
+   version='1.0',
+   description='A library for spoofing the Qobuz web player',
+   author='DashLt',
+   packages=['spoofbuz'],
+)
